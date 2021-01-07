bdcec65ceed4dd28c93cbe818933534c66e02dab
Cristiano <cristiano.tarconi@gmail.com>
2021-01-07 16:27:33 +0100

    jquery functions

---
+++ main.js
@@ -0,0 +1,19 @@
+// Istruzione principale per assicurarci che il codice js venga caricato solo a documento pronto
+$(document).ready(function(){
+
+    // Creo variabili per semplificare codice
+    var btnShow = $('.header-right a');
+    var btnHamb = $('.hamburger-menu');
+    var btnClose = $('.hamburger-menu .close');
+
+    // Funzione per mostrare menù
+    btnShow.click(function(){
+        btnHamb.addClass('active');
+    });
+
+    // Funzione per nascondere menù
+    btnClose.click(function(){
+        btnHamb.removeClass('active');
+    });
+
+});
